97a61d7c01831c5c3a2cae6ab452acc576f891d5
Nyambevos <ruslan.bilokoniuk@gmail.com>
2024-04-20 22:56:59 +0300

    Add users model in database

--- src/database/models.py
+++ src/database/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, func
+from sqlalchemy import Column, Integer, String, func, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.sqltypes import Date, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -13,3 +14,15 @@ class Contact(Base):
     phone = Column(String(15), nullable=False)
     birthday = Column(Date)
     created_at = Column('created_at', DateTime, default=func.now())
+    user_id = Column('user_id', ForeignKey('users.id', ondelete='CASCADE'), default=None)
+    user = relationship('User', backref="notes")
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    created_at = Column('crated_at', DateTime, default=func.now())
+    avatar = Column(String(255), nullable=True)
+    refresh_token = Column(String(255), nullable=True)
