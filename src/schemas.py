d330e1e34d43bd31e9f8acebcd9bd1f5cf1797b9
Nyambevos <ruslan.bilokoniuk@gmail.com>
2024-04-20 22:58:20 +0300

    Add users schemas

--- src/schemas.py
+++ src/schemas.py
@@ -1,5 +1,4 @@
-from datetime import date
-from typing import List, Optional
+from datetime import date, datetime
 from pydantic import BaseModel, Field, EmailStr
 
 class ContactModel(BaseModel):
@@ -14,3 +13,31 @@ class ContactResponse(ContactModel):
 
     class Config:
         orm_mode = True
+
+
+class UserModel(BaseModel):
+    username: str = Field(min_length=5, max_length=16)
+    email: str
+    password: str = Field(min_length=6, max_length=10)
+
+
+class UserDb(BaseModel):
+    id: int
+    username: str
+    email: str
+    created_at: datetime
+    avatar: str
+
+    class Config:
+        orm_mode = True
+
+
+class UserResponse(BaseModel):
+    user: UserDb
+    detail: str = "User successfully created"
+
+
+class TokenModel(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
